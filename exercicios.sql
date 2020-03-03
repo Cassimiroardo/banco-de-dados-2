24565b2982ea9f20b514bad2c49ec436bacf3fd9
Eduardo Cassimiro <56744175+Cassimiroardo@users.noreply.github.com>
2020-03-03 09:39:01 -0300

    Update exercicios.sql

--- exercicios.sql
+++ exercicios.sql
@@ -4,13 +4,13 @@ USE compubras;
 -- código a partir da tabela item de pedido.       
 
 SELECT b.CodPedido,
-	   a.Descricao,
+       a.Descricao,
        SUM(b.quantidade) AS total
   FROM produto AS a
   INNER JOIN itempedido AS b
   WHERE a.CodProduto = b.CodProduto
   GROUP BY b.CodPedido,
-		   a.CodProduto;
+           a.CodProduto;
   
 -- 2 | Listar a quantidade de produtos que cada pedido contém.
 
